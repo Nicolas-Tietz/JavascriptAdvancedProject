35ac9381b293c28d023cf6ee58a81c166a597118
Nicolas Tietz <nicolastietz48@gmail.com>
2023-05-13 13:31:52 +0200

    Added a parent div to the spinner div to avoid conflicts with the animation

--- src/components/loadingNewsCube/loading.js
+++ src/components/loadingNewsCube/loading.js
@@ -2,11 +2,13 @@
 import './styles.scss';
 
 function loadingBtn(){
+    const parentDiv = document.createElement('div');
+    parentDiv.className = 'spinner-parent';
     const spinnerDiv = document.createElement('div');
     spinnerDiv.className = 'spinner';
     spinnerDiv.innerHTML = '<div></div><div></div><div></div><div></div><div></div><div></div>'
-    
-    return spinnerDiv;
+    parentDiv.appendChild(spinnerDiv);
+    return parentDiv;
 }
 
 export default loadingBtn;
